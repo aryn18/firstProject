d6b1069b857968da4b79df46a1e94c3de8eb5101
Aryan <aryan.gupta_cs19@gla.ac.in>
2023-01-11 19:29:02 +0530

    type function

---
+++ .idea/typeFunction.py
@@ -0,0 +1,19 @@
+a = 10
+print(type(a))
+b = 10.5
+print(type(b))
+c = 2+ 4j
+print(type(c))
+d = None
+print(type(d))
+
+str = "ary"
+print(type(str))
+l = [10, 20 , 30]
+print(type(l))
+t = (10, 20 , 30)
+print(type(t))
+s = {10, 20 , 30}
+print(type(s))
+d = {10: "ary", 20: "repo"}
+print(type(d))
