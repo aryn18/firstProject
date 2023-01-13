baed73b3060150b879307dcc79ccf5bb36a456f3
Aryan <aryan.gupta_cs19@gla.ac.in>
2023-01-13 13:07:30 +0530

    print function working

---
+++ printFunction.py
@@ -0,0 +1,15 @@
+# example 1
+
+print("Hello")
+print("Welcome", "to", "Python repo")
+print()
+print("Hope you are doing well")
+
+# example 2
+print("welcome", end=" ")
+# end as a by default value if we didn't pass end compiler gives self space.
+print("to repo")
+
+print("25", "08", "2020", sep="-")
+print(10, 20 , 30, sep="+", end=" ")
+print(40, 50)
