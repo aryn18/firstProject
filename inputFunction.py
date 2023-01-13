19c10123a35ec6309e98b367fca35d1216db5e8f
Aryan <aryan.gupta_cs19@gla.ac.in>
2023-01-13 18:21:21 +0530

    input function and how can input thier data in console.

---
+++ inputFunction.py
@@ -0,0 +1,21 @@
+# example 1
+name = input("Enter your name: ")
+print("Welcome " + name)
+
+# example 2
+name1 = input("Enter Your name")
+age = input("Enter your age: ")
+print("your age is " + age)
+
+# example 3
+x = input("Enter your number one: ")
+y = input("Enter your number two: ")
+x = int(x)
+y = int(y)
+res = x + y
+print("sum is", res)
+
+a = int(input("Enter your number one: "))
+b = int(input("Enter your number two: "))
+res = a + b
+print(res)
