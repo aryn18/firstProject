6844dc06b4e9c3632b42ba373ce9c169f58e66bc
Aryan <aryan.gupta_cs19@gla.ac.in>
2023-01-13 19:33:43 +0530

    airthmetic operators and their examples

---
+++ airthmeticOperator.py
@@ -0,0 +1,42 @@
+x = 9
+y = 4
+# addition operator
+print(x+y)
+
+# subtraction operator
+print(x-y)
+
+# multiplication operator
+print(x*y)
+
+# division operator
+print(x/y)
+
+# division operator we get floor of a number ex 3.3 is 3 5.9 is 5 for negative is -3.2 is -4  and floor - 9.5 is -10.
+print(x//y)
+
+# modulo operator
+print(x % y)
+
+# power operator or exponent operator
+print(x**y)
+
+# example 2
+a = -5
+b = 2
+print(a//b)
+c = 5.0
+d = 2
+print(c//d)
+e = 2
+f = -2
+print(e**f)
+
+# precedence example
+print(5+2*3)
+print(5+3*4**2)
+
+# associativity example
+print(5+4-2)
+print(2**2**-1)
+print((2**2)**-1)
